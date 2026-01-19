d88b3196a1b60960caeb2d9ba7eb55df7b78bca8
gulzarhussaindev <gulzarhussain@uol.ac.in>
2026-01-19 12:07:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    Author: Gulzar Hussain
 ===================================================== */
 
-const CACHE_NAME = "uol-directory-v3";
+const CACHE_NAME = "uol-directory-v4";
 
 const STATIC_ASSETS = [
   "./",
